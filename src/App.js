ad386b971e4df781cd565a5ead124ec1fc48ff3e
Eric Gonzalez <ericegonz.se@gmail.com>
2021-02-28 23:06:19 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Navbar/Navbar'
+import Home from './Home/Home'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import BlogCreate from './BlogCreate/BlogCreate';
+import BlogDetails from './BlogDetails/BlogDetails';
+import Error from './Error/Error'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <BlogCreate />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+            <Route path="*">
+              <Error errorMessage="Page not found"/>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
